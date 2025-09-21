7a15f7ca1b80b454f90daf605456f8aca084f603
abelmekonen <abelmekonen247@gmail.com>
2025-09-21 23:45:10 +0300

    Refactor project structure: reintroduce core files including settings, URLs, ASGI, WSGI, and Celery configuration

--- alx_travel_app/listings/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import JsonResponse
-from rest_framework.decorators import api_view
-
-@api_view(['GET'])
-def ping(request):
-    return JsonResponse({'status': 'ok', 'app': 'alx_travel_app'})
